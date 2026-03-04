6f24c0a7205f45a1f5cd846d3e400ff770bca2b1
Juan Pablo Rolo <juanpablorolo2007@gmail.com>
2026-03-04 13:41:27 +0000

    a

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const repositoryName = process.env.GITHUB_REPOSITORY?.split('/')[1]
-
-const normalizeBasePath = (value) => {
-  if (!value) return '/'
-  return value.endsWith('/') ? value : `${value}/`
-}
-
-const basePath = normalizeBasePath(
-  process.env.VITE_BASE_PATH || (repositoryName ? `/${repositoryName}` : '/'),
-)
-
 export default defineConfig({
+  base: '/Ames/',
   plugins: [react()],
-  base: basePath,
 })
